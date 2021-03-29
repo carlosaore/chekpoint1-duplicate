27c494c50db2d9a089e527ce9d21dae0a0634af8
Manuel Marque <manuelmarq05@gmail.com>
2021-03-29 15:36:45 +0200

    change backgroundColor with prompt

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,10 +8,13 @@ image.addEventListener("click", function(event) {
 //change avatar
 const btn = document.querySelector("#btnNameColor");
 const surName = document.querySelector("#name");
+const colorBack = document.querySelector(".pink-bg");
 
 
 btn.addEventListener("click", function(event) {
    surName.innerHTML = prompt("type Name");
    surName.style.color = "white";
+   colorBack.style.backgroundColor = prompt("type color");
+   //colorBack.style.backgroundColor = colorBack;
 });
 
