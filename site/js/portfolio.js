9e0795bcf3c330d2e9ff2a0c54568dec01e8b024
Manuel Marque <manuelmarq05@gmail.com>
2021-03-29 15:21:16 +0200

    add I can replace the text Matt on the paragraphe with your name and it's color with the color white

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,14 @@ const image = document.querySelector("#imgavatar");
 image.addEventListener("click", function(event) {
   image.src = "image/avatar.svg"
 });
+
+//change avatar
+const btn = document.querySelector("#btnNameColor");
+const surName = document.querySelector("#name");
+
+
+btn.addEventListener("click", function(event) {
+   surName.innerHTML = prompt("type Name");
+   surName.style.color = "white";
+});
+
