9e5c34c837ff8235887c4a4d5474922fc23dfb12
Sergio Caballero <s.caballero1993@gmail.com>
2021-03-29 17:01:25 +0200

    finished

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,26 +1,33 @@
+let devTools=[Github,VScode,Terminal]
+
 const avatar= document.getElementById("avatar");
 const button= document.getElementById("but-name");
+const modButton=document.getElementById("mod-dev")
 
 
 avatar.addEventListener('click', function(){
     avatar.src= "image/avatar.svg"
 })
 
+
+
 button.addEventListener('click', function(){
     const name= document.getElementById("name");
     const color= document.getElementById("presentation-container");
-    const colorText1= document.getElementById("text1");
-    const colorText2= document.getElementById("text2");
-    const colorText3= document.getElementById("text3");
-    const colorText4= document.getElementById("text4");
+    const text=document.getElementsByClassName("pink-text")
+    const links = document.getElementsByTagName("a");
+
+
 
 
     let newColor=prompt("Introduce a new color");
-    colorText1.style.color=newColor;
-    colorText2.style.color=newColor;
-    colorText3.style.color=newColor;
-    colorText4.style.color=newColor;
     color.style.background=newColor;
+    for (let j = 0; j < text.length; j++) {
+        text[j].style.color = newColor;
+    }
+    for (let i = 0; i < links.length; i++) {
+        links[i].style.color = newColor;
+    }
    
     
     let newName= prompt("Introduce your name");
