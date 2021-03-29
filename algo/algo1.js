2599fc4a0c215b83c4d2ff761bfc1fd873ce2e2f
Manuel Marque <manuelmarq05@gmail.com>
2021-03-29 11:50:11 +0200

    exercise 1 done in algo1.js

--- algo/algo1.js
+++ algo/algo1.js
@@ -8,6 +8,9 @@ thirdAngle(20, 80) should return 80
 
 function thirdAngle(a, b) {
   // Your code here !
+  //the two angles are added and subtracted with 180 which is the total sum of the angles 
+  rest = 180 - (a + b);
+  return rest
 }
 
 console.log(thirdAngle(90, 30));
