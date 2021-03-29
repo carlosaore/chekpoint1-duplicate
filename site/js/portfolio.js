1762f63e357d5a0c6609a105bacd4e8833b84b48
Manuel Marque <manuelmarq05@gmail.com>
2021-03-29 16:16:07 +0200

    replace all the elements in pink by the color:

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,13 +8,19 @@ image.addEventListener("click", function(event) {
 //change avatar
 const btn = document.querySelector("#btnNameColor");
 const surName = document.querySelector("#name");
-const colorBack = document.querySelector(".pink-bg");
+const colorBack = document.querySelectorAll(".pink-bg");
+const colorText = document.querySelectorAll(".pink-text");
 
 
 btn.addEventListener("click", function(event) {
    surName.innerHTML = prompt("type Name");
    surName.style.color = "white";
-   colorBack.style.backgroundColor = prompt("type color");
-   //colorBack.style.backgroundColor = colorBack;
+   let valor = prompt("type color");
+   for(let i = 0; i < colorBack.length;i++){
+    colorBack[i].style.backgroundColor = valor;
+  }
+   for(let j = 0; j < colorText.length;j++){
+    colorText[j].style.color = valor;
+   }
 });
 
